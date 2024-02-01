09de3ba69db8a08a8565ca0590886ad90ef42fe0
NetherCraftMC <github@nethercraft.de>
2024-02-01 15:08:12 +0100

    CHANGES

--- src/main/java/de/ghost143/xplotsquared/plotsquared/XCredits.java
+++ src/main/java/de/ghost143/xplotsquared/plotsquared/XCredits.java
@@ -5,16 +5,17 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class XInfo implements CommandExecutor {
+public class XCredits implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             player.sendMessage("§8-*-*-*-*-*-*-*-*-*");
-            player.sendMessage("§4§lXInfo §7» §aBUILD VERSION§7: §bSNAPSHOT");
-            player.sendMessage("§4§lXInfo §7» §aXPlotSquared made by §bGhost143");
-            player.sendMessage("§4§lXInfo §7» §afor §bPlotSquared §66.1.1");
+            player.sendMessage("§a§lXCredits §7» §aBUILD VERSION§7: §bSNAPSHOT");
+            player.sendMessage("§a§lXCredits §7» §aXPlotSquared made by §bGhost143");
+            player.sendMessage("§8-*-*-*-*-*-*-*-*-*");
+
             player.sendMessage("§8-*-*-*-*-*-*-*-*-*");
         } else {
             sender.sendMessage("§a§lKitFFA §7» §4Fehler!§7, §cBeim Ausführen des Commands§b....");
